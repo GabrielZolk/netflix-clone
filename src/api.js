b879ac9b21876da39f256bdba1f850d32583d553
GabrielEagle <dialixkat@gmail.com>
2022-12-03 19:23:44 -0300

    integracao da api

---
+++ src/api.js
@@ -0,0 +1,39 @@
+const API_KEY = "b82e94dabead56f01c73ef2fa8d7b810"
+
+const categories = [
+    {
+        name: "trending",
+        title: "Em alta",
+        path: "/trending/all/week?api_key=${API_KEY}&language=pt-BR",
+    },
+    {
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: "/discover/tv?api_key=${API_KEY}&with_network=213",
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: "/movie/top_rated?api_key=${API_KEY}&language=pt-BR",
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: "/discover/tv?api_key=${API_KEY}&with_genres=1074",
+    },
+    {
+        name: "documentaries",
+        title: "Documentários",
+        path: "/discover/tv?api_key=${API_KEY}&with_genres=99",
+    },
+];
+
+export const getMovies = async (path) => {
+    try {
+        let url = `https://api.themoviedb.org/3/${path}`
+        const response = await fetch(url);
+        return await response.json();  
+    } catch (error) {
+        console.log("error getMovies: ", error)
+    } 
+}
