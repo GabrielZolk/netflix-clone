69c858e893c48744f3f926cacf24e5cf0b0aeae6
GabrielEagle <dialixkat@gmail.com>
2022-12-04 13:25:54 -0300

    criação do componente ROW (parte funcional)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import categories from './api';
 import './App.css';
+import Row from "./components/Row"
 
 function App() {
   return (
     <div className="App">
-     {/*Navbar*/}
-     {/*Destaques*/}
-     {/*Em alta*/}
-     {/*Filmes por categoria*/}
+      {/*Navbar*/}
+      {/*Destaques*/}
+      {/*Em alta*/}
+
+      {/*Filmes por categoria*/}
+      {categories.map((category) => {
+        return (
+          <Row
+            key={category.name}
+            title={category.title}
+            path={category.path}
+          />
+        );
+      })}
     </div>
   );
 }
